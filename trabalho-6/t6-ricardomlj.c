2f5c6db914b24a7c0acf3cb2e6063164f22adeab
Ricardo Mendes <ricardomlealjr@gmail.com>
2018-12-13 15:12:31 -0200

    Enunciado e testes T5 e T6

---
+++ trabalho-6/t6-ricardomlj.c
@@ -0,0 +1,18 @@
+/* ==========================================================================
+ * Universidade Federal de São Carlos - Campus Sorocaba
+ * Disciplina: Algoritmos em Grafos
+ * Profa. Cândida Nunes da Silva
+ *
+ * Trabalho 06 - Arbitragem
+ *
+ * RA: 562262
+ * Aluno: Ricardo Mendes Leal Junior
+ * ========================================================================== */
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
